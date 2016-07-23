e9db05e082f9c12ea1d8904e498c2c12e6cda7c7
alexmarcovich <alex.carmona@terra.com>
2016-07-23 01:22:35 -0500

    Create credentials.js

---
+++ src/shared/credentials.js
@@ -0,0 +1,7 @@
+module.exports = Object.freeze({
+    GOOGLE_CLIENT_ID : '< 293291478750-tb7i6knf9h5qhna6f4p8ngrar9l737oh.apps.googleusercontent.com >',
+    GOOGLE_CLIENT_SECRET: '< nxMkVwt3AoMX7YU7GQnKP_d4 >'
+})
+
+npm run-script install-all
+npm start
